988dd3bf2aa5eec231728a6e92faebf3049678a9
kotahorii <khorii.micron@gmail.com>
2022-01-21 21:42:39 +0900

    [update] test.dart

--- test.dart
+++ test.dart
@@ -7,3 +7,8 @@ void func1(String? param1, [int param2 = 0]) {
   var result = '$param1/$param2';
   print(result);
 }
+
+void func2(String? param1, int param2) => print('$param1/$param2');
+
+void hello() => print('Hello');
+void func3(f) => f();
